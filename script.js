79eec13a7a443dde3ffea8a58dcbb4fd0bab2a14
raed-az <azouziraed34@gmail.com>
2025-03-18 16:28:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -87,14 +87,14 @@ async function handleUserMessage(userMessage) {
     if (userMessage === "/dhia") {
         document.getElementById("hed").style.backgroundColor = "pink";
         document.getElementById("toptitl").style.backgroundColor = "pink";
-        document.getElementById("chat").style.background = "url(dhia.png) center no-repeat";
+        document.getElementById("chat").style.background = "url(dhiaa.png) center no-repeat";
         document.getElementById("chat").style.backgroundSize = "cover";
         
         let audio = document.getElementById("dhiaAudio");
         if (!audio) {
             audio = document.createElement("audio");
             audio.id = "dhiaAudio";
-            audio.src = "dhia.mp3";
+            audio.src = "dhiaa.mp3";
             audio.loop = true;
             document.body.appendChild(audio);
         }
